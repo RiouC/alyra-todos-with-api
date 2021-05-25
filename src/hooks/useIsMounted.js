44a2633424f0f3bfc0dbfb6ccc75b648d66438a3
RiouC <riou.christophe@protonmail.com>
2021-05-25 10:31:32 +0200

    [hooks] useIsMounted

---
+++ src/hooks/useIsMounted.js
@@ -0,0 +1,9 @@
+// src/hooks/useIsMounted.js
+export const useIsMounted = () => {
+  const isMounted = useRef(false)
+  useEffect(() => {
+    isMounted.current = true
+    return () => (isMounted.current = false)
+  }, [])
+  return isMounted
+}
